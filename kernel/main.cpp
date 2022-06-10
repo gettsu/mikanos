a62e788c370836f537e3fa5be78e838fc5e67d04
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2022-06-10 20:03:08 +0900

    osbook_day17

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -28,6 +28,7 @@
 #include "keyboard.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
+#include "fat.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -107,64 +108,13 @@ void InputTextWindow(char c) {
   layer_manager->Draw(text_window_layer_id);
 }
 
-std::shared_ptr<ToplevelWindow> task_b_window;
-unsigned int task_b_window_layer_id;
-void InitializeTaskBWindow() {
-  task_b_window = std::make_shared<ToplevelWindow>(
-      160, 52, screen_config.pixel_format, "TaskB Window");
-
-  task_b_window_layer_id = layer_manager->NewLayer()
-    .SetWindow(task_b_window)
-    .SetDraggable(true)
-    .Move({100, 100})
-    .ID();
-
-  layer_manager->UpDown(task_b_window_layer_id, std::numeric_limits<int>::max());
-}
-
-void TaskB(uint64_t task_id, int64_t data) {
-  printk("TaskB: task_id=%lu, data=%lu\n", task_id, data);
-  char str[128];
-  int count = 0;
-
-  __asm__("cli");
-  Task& task = task_manager->CurrentTask();
-  __asm__("sti");
-
-  while (true) {
-    ++count;
-    sprintf(str, "%010d", count);
-    FillRectangle(*task_b_window->InnerWriter(), {20, 4}, {8 * 10, 16}, {0x50, 0x49, 0x46});
-    WriteString(*task_b_window->InnerWriter(), {20, 4}, str, {255, 255, 255});
-    Message msg{Message::kLayer, task_id};
-    msg.arg.layer.layer_id = task_b_window_layer_id;
-    msg.arg.layer.op = LayerOperation::Draw;
-    __asm__("cli");
-    task_manager->SendMessage(1, msg);
-    __asm__("sti");
-
-    while (true) {
-      __asm__("cli");
-      auto msg = task.ReceiveMessage();
-      if (!msg) {
-        task.Sleep();
-        __asm__("sti");
-        continue;
-      }
-
-      if (msg->type == Message::kLayerFinish) {
-        break;
-      }
-    }
-  }
-}
-
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
 extern "C" void KernelMainNewStack(
     const FrameBufferConfig& frame_buffer_config_ref,
     const MemoryMap& memory_map_ref,
-    const acpi::RSDP& acpi_table) {
+    const acpi::RSDP& acpi_table,
+    void* volume_image) {
   MemoryMap memory_map{memory_map_ref};
 
   InitializeGraphics(frame_buffer_config_ref);
@@ -183,7 +133,6 @@ extern "C" void KernelMainNewStack(
   InitializeLayer();
   InitializeMainWindow();
   InitializeTextWindow();
-  InitializeTaskBWindow();
   layer_manager->Draw({{0, 0}, ScreenSize()});
 
   acpi::Initialize(acpi_table);
@@ -196,10 +145,6 @@ extern "C" void KernelMainNewStack(
 
   InitializeTask();
   Task& main_task = task_manager->CurrentTask();
-  const uint64_t taskb_id = task_manager->NewTask()
-    .InitContext(TaskB, 45)
-    .Wakeup()
-    .ID();
   const uint64_t task_terminal_id = task_manager->NewTask()
     .InitContext(TaskTerminal, 0)
     .Wakeup()
@@ -208,7 +153,6 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
-  active_layer->Activate(task_b_window_layer_id);
 
   char str[128];
 
@@ -254,16 +198,19 @@ extern "C" void KernelMainNewStack(
     case Message::kKeyPush:
       if (auto act = active_layer->GetActive(); act == text_window_layer_id) {
         InputTextWindow(msg->arg.keyboard.ascii);
-      } else if (act == task_b_window_layer_id) {
-        if (msg->arg.keyboard.ascii == 's') {
-          printk("sleep TaskB: %s\n", task_manager->Sleep(taskb_id).Name());
-        } else if (msg->arg.keyboard.ascii == 'w') {
-          printk("wakeup TaskB: %s\n", task_manager->Wakeup(taskb_id).Name());
-        }
       } else {
-        printk("key push not handled: keycode %02x, ascii %02x\n",
-            msg->arg.keyboard.keycode,
-            msg->arg.keyboard.ascii);
+        __asm__("cli");
+        auto task_it = layer_task_map->find(act);
+        __asm__("sti");
+        if (task_it != layer_task_map->end()) {
+          __asm__("cli");
+          task_manager->SendMessage(task_it->second, *msg);
+          __asm__("sti");
+        } else {
+          printk("key push not handled: keycode %02x, ascii %02x\n",
+              msg->arg.keyboard.keycode,
+              msg->arg.keyboard.ascii);
+        }
       }
       break;
     case Message::kLayer:
